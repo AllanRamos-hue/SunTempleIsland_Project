85d2e71c3a4c3b88f5374005d41be467bbef122c
hiTougo <brunoborgesguadacholi@gmail.com>
2021-05-17 23:57:33 -0300

    GameOver

--- HUD/Assets/Scripts/GameOverScreen.cs
+++ HUD/Assets/Scripts/GameOverScreen.cs
@@ -1,8 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameOverScreen : MonoBehaviour
 {
- 
+    private PlayerLife player;
+    public GameObject gameOver;
+
+
+    void Start()
+    {
+        player = GetComponent<PlayerLife>();
+
+    }
+
+    void Update()
+    {
+        if (player.life <= 0) 
+        {
+            gameOver.SetActive(true);
+        }
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void ReturnMenu()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
 }
